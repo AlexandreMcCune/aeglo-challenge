7a9d38c1e7625cc5f4696c26fb0b9f88dd377c0e
Alexandre Mc Cune <alexandre.mccune@gmail.com>
2019-05-30 22:57:58 -0400

    main

--- script/main.js
+++ script/main.js
@@ -2,14 +2,15 @@ const challenge1 = require('../script/challenge1');
 const challenge2 = require('../script/challenge2');
 
 
-resultChallenge1 = challenge1('aaaa string bbbb', 'string');
+resultChallenge1 = challenge1('She believed', 'He lied');
 
 console.log('Challenge 1');
 console.log(resultChallenge1);
 
+console.log('\n');
 
 
-resultChallenge2 = challenge2('');
+resultChallenge2 = challenge2('Hello World in a frame');
 
 console.log('Challenge 2');
 console.log(resultChallenge2);
